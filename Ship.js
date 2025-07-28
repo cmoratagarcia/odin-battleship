ace3902fd53750ecb52018d2a6d3635fa2164b06
Carmen Morata <cmoratagarcia@gmail.com>
2025-07-28 11:10:42 -0700

    Create hit and isSunk functions inside of Ship

--- Ship.js
+++ Ship.js
@@ -1,16 +1,24 @@
 //1.Begin your app by creating the Ship class/factory (your choice).
 
 function Ship(length) {
- 
   let hits = 0;
-  let isSunk = false;
-  return { length, hits, isSunk };
+
+  //Ships should have a hit() function that increases the number of ‘hits’ in your ship.
+  function hit() {
+    hits++;
+  }
+  //isSunk() should be a function that calculates whether a ship is considered sunk
+  function isSunk() {
+    return hits >= length;
+  }
+  return {
+    length,
+    isSunk,
+    hit,
+    get hits() {
+      return hits;
+    },
+  };
 }
-//Your ‘ships’ will be objects that include their length, the number of times they’ve been hit and whether or not they’ve been sunk.
-
-//Ships should have a hit() function that increases the number of ‘hits’ in your ship.
-
-//isSunk() should be a function that calculates whether a ship is considered sunk
 
 module.exports = Ship;
-
--- Ship.test.js
+++ Ship.test.js
@@ -1,5 +1,20 @@
 const Ship = require("./Ship");
 
-test("returns object with parameters", () => {
-  expect(Ship(3)).toEqual({ length: 3, hits: 0, isSunk: false });
+test("creates a ship with specified length", () => {
+  const ship = Ship(3);
+  expect(ship.length).toBe(3);
+});
+
+test("hit increments hits", () => {
+  const ship = Ship(2);
+  ship.hit();
+  expect(ship.hits).toBe(1);
+});
+
+test("isSunk returns true when sunk", () => {
+  const ship = Ship(3);
+  ship.hit();
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
 });
