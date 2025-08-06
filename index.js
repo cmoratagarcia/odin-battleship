e0fc75f2a7f6c93a10cb2dc97042c5043bcaeb5b
Carmen Morata <cmoratagarcia@gmail.com>
2025-08-06 10:39:35 -0700

    Update initGame to also render ships

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import GameController from "./GameController.js";
-import createBoard from "./UI.js";
+import { createBoard, renderShips } from "./UI.js";
 
 function initGame() {
+  const game = GameController();
+
+  // Create boards
   createBoard("player1-board");
   createBoard("player2-board");
+
+  const player1Container = document.getElementById("player1-board");
+  const player2Container = document.getElementById("player2-board");
+
+  // Render ships
+  renderShips(game.player1.board.getShips(), player1Container);
+  renderShips(game.player2.board.getShips(), player2Container);
 }
 
 initGame();
