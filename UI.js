fb0e5c55cd5e5095453bd1049ed2849ac3e5fed7
Carmen Morata <cmoratagarcia@gmail.com>
2025-08-06 10:36:56 -0700

    Add renderShips function to display them on the board

--- UI.js
+++ UI.js
@@ -12,4 +12,17 @@ export default function createBoard(containerID) {
       container.appendChild(cell);
     }
   }
+
+}
+export function renderShips(fleet, containerID) {
+  fleet.forEach((shipObj) => {
+    shipObj.positions.forEach(([x, y]) => {
+      const cell = containerID.querySelector(
+        `[data-x="${x}"][data-y="${y}"]`
+      );
+      if (cell) {
+        cell.classList.add("ship-cell");
+      }
+    });
+  });
 }
